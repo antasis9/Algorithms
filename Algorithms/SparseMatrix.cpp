4c7ecbf677976b3b23cd0d79a7654eb08564c4a5
antasis9 <antasis9@gmail.com>
2015-06-29 00:22:41 +0900

    Transpose 구현

--- Algorithms/SparseMatrix.cpp
+++ Algorithms/SparseMatrix.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+SparseMatrix::SparseMatrix(int rows, int columns) {
+	this->rows = rows;
+	this->columns = columns;
+}
 
 SparseMatrix::SparseMatrix(int rows, int columns, int** items)
 {
@@ -21,7 +25,7 @@ SparseMatrix::SparseMatrix(int rows, int columns, int** items)
 				term->col = column;
 				term->value = item;
 
-				this->items[this->terms++] = *term;
+				this->items[this->terms++] = term;
 			}
 		}
 	}
@@ -33,15 +37,73 @@ SparseMatrix::~SparseMatrix()
 	this->rows = 0;
 	this->columns = 0;
 	
-	/*for (int i = 0; i < terms; i++) {
-		delete &(items[i]));
-	}*/
+	for (int i = 0; i < terms; i++) {
+		MatrixTerm* term = this->items[i];
+
+		delete term;
+	}
 }
 
 
-SparseMatrix SparseMatrix::Transpose()
+SparseMatrix* SparseMatrix::Transpose()
 {
-	return *this;
+	SparseMatrix* transposeMatrix = new SparseMatrix(columns, rows);
+
+	for (int column = 0; column < this->columns; column++) {
+		for (int i = 0; i < terms; i++) {
+			MatrixTerm* term = this->items[i];
+		
+			if (term->col == column) {
+				MatrixTerm* newTerm = new MatrixTerm();
+				newTerm->row = term->col;
+				newTerm->col = term->row;
+				newTerm->value = term->value;
+
+				transposeMatrix->items[transposeMatrix->terms++] = newTerm;
+			}
+		}
+	}
+
+	return transposeMatrix;
+}
+
+SparseMatrix* SparseMatrix::FastTranspose() {
+	SparseMatrix* transposeMatrix = new SparseMatrix(columns, rows);
+
+	int* rowSize = new int[columns];
+	int* rowStart = new int[columns];
+
+	for (int i = 0; i < columns; i++) {
+		rowSize[i] = 0;
+	}
+
+	for (int i = 0; i < terms; i++) {
+		rowSize[this->items[i]->col]++;
+	}
+
+	rowStart[0] = 0;
+	for (int i = 1; i < columns; i++) {
+		rowStart[i] = rowStart[i - 1] + rowSize[i - 1];
+	}
+
+	for (int i = 0; i < terms; i++) {
+		int j = rowStart[items[i]->col];
+
+		MatrixTerm* newTerm = new MatrixTerm();
+		newTerm->row = items[i]->col;
+		newTerm->col = items[i]->row;
+		newTerm->value = items[i]->value;
+
+		transposeMatrix->items[j] = newTerm;
+		transposeMatrix->terms++;
+
+		rowStart[items[i]->col]++;
+	}
+
+	delete[] rowSize;
+	delete[] rowStart;
+
+	return transposeMatrix;
 }
 
 
@@ -51,19 +113,39 @@ SparseMatrix SparseMatrix::Add(SparseMatrix b)
 }
 
 
-SparseMatrix SparseMatrix::Multiply(SparseMatrix b)
+SparseMatrix* SparseMatrix::Multiply(SparseMatrix b)
 {
-	return b;
-}
+	if (columns != b.columns) {
+		cout << "Incompatible matrices" << endl;
+		return new SparseMatrix(0, 0);
+	}
 
+	SparseMatrix* matrix = new SparseMatrix(rows, columns);
+
+	SparseMatrix* bXpose = b.FastTranspose();
+
+	for (int row = 0; row < rows; rows++) {
+		int value = 0;
+
+		for (int column = 0; column < columns; column++) {
+			for (int term = 0; term < b.terms; term++) {
+
+			}
+		}
+	}
+
+	delete bXpose;
+
+	return matrix;
+}
 
 void SparseMatrix::Dump()
 {
 	cout << "Rows: " << rows << ", Columns: " << columns << ", Terms: " << terms << endl;
 
 	for (int i = 0; i < terms; i++) {
-		MatrixTerm term = items[i];
+		MatrixTerm* term = items[i];
 
-		cout << "Row:\t" << term.row << ", Column:\t" << term.col << ", Value:\t" << term.value << endl;
+		cout << "Row:\t" << term->row << ", Column:\t" << term->col << ", Value:\t" << term->value << endl;
 	}
 }
--- Algorithms/SparseMatrix.h
+++ Algorithms/SparseMatrix.h
@@ -1,22 +1,24 @@
 #pragma once
 #include "MatrixTerm.h"
 
-#define	MAX_ITEM_COUNTS	100
+#define	MAX_ITEM_COUNTS	10000
 
 class SparseMatrix
 {
 public:
+	SparseMatrix(int rows, int columns);
 	SparseMatrix(int rows, int columns, int** items);
 	~SparseMatrix();
-	SparseMatrix Transpose();
+	SparseMatrix* Transpose();
+	SparseMatrix* FastTranspose();
 	SparseMatrix Add(SparseMatrix b);
-	SparseMatrix Multiply(SparseMatrix b);
+	SparseMatrix* Multiply(SparseMatrix b);
 	void Dump();
 
 private:
 	int rows = 0;
 	int columns = 0;
 	int terms = 0;
-	MatrixTerm items[MAX_ITEM_COUNTS];
+	MatrixTerm* items[MAX_ITEM_COUNTS];
 };
 
