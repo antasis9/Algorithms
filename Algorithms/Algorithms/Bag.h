fa9f1b6518b0ada2cafcb24b2d0e5b505632b529
antasis9 <antasis9@gmail.com>
2015-07-08 00:08:16 +0900

    Implement Bag

--- Algorithms/Algorithms/Bag.h
+++ Algorithms/Algorithms/Bag.h
@@ -1,18 +1,91 @@
 #pragma once
 
+#include <iostream>
+using namespace std;
+
 template <class T>
 class Bag
 {
 public:
-	Bag();
+	Bag(int);
 	~Bag();
 	void Add(const T& item);
 	T* Delete(T& item);
-	Boolean IsFull();
-	Boolean IsEmpty();
+	bool IsFull();
+	bool IsEmpty();
 private:
 	void Full();
 	void Empty();
 	T* items;
 	int top;
+	int maxSize;
 };
+
+template <class T>
+Bag<T>::Bag(int maxSize)
+{
+	this->maxSize = maxSize;
+	this->top = -1;
+	this->items = new T[this->maxSize];
+}
+
+template <class T>
+Bag<T>::~Bag()
+{
+	delete[] this->items;
+}
+
+template <class T>
+void Bag<T>::Add(const T& item)
+{
+	if (IsFull()) {
+		Full();
+	}
+	else {
+		items[++top] = item;
+	}
+}
+
+template <class T>
+T* Bag<T>::Delete(T& item)
+{
+	if (IsEmpty()) {
+		Empty();
+	}
+	else {
+		int deleteIndex = top / 2;
+		int value = items[deleteIndex];
+
+		for (int index = deleteIndex; i < top; i++) {
+			items[index] = items[index + 1];
+		}
+
+		top--;
+
+		return &value;
+	}
+}
+
+template <class T>
+bool Bag<T>::IsFull()
+{
+	return maxSize == top + 1;
+}
+
+template <class T>
+bool Bag<T>::IsEmpty()
+{
+	return top == -1;
+}
+
+template <class T>
+void Bag<T>::Full()
+{
+	cout << "OnFull" << endl;
+}
+
+template <class T>
+void Bag<T>::Empty()
+{
+	cout << "OnEmpty" << endl;
+}
--- Algorithms/Main.cpp
+++ Algorithms/Main.cpp
@@ -2,6 +2,7 @@
 #include "time.h"
 #include "SparseMatrix.h"
 #include "Algorithms\String.h"
+#include "Algorithms\Bag.h"
 
 using namespace std;
 
@@ -9,12 +10,20 @@ using namespace std;
 #define	COLUMNS	38
 
 int main() {
-	String content("afdsafsdfsdvxzaabcabcacadabcabcacabaaaa");
+	/*String content("afdsafsdfsdvxzaabcabcacadabcabcacabaaaa");
 	String pattern("abcabcacab");
 
 	int index = content.Search(&pattern);
 
-	cout << "Index: " << index << endl;
+	cout << "Index: " << index << endl;*/
+
+	Bag<int> bag(5);
+	bag.Add(10);
+	bag.Add(11);
+	//bag.Add(12);
+	//bag.Add(13);
+	//bag.Add(14);
+	//bag.Add(15);
 
 	return 0;
 }
